af7470195461ffc9856acaefa29726c5ea8847c1
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-05-06 18:35:19 -0300

    Curso de Java da Alura

---
+++ screenmatch/src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.model.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    public void inclui(Titulo titulo) {
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
